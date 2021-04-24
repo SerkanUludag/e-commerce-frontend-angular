a09a5c001d07d31c21a8bdad2b8afafe8cf13bf9
SerkanUludag <hsuluda1@gmail.com>
2021-04-24 16:03:01 +0300

    add product form

---
+++ src/app/components/product-add/product-add.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { CartItem } from 'src/app/models/cartItem';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.css'],
+})
+export class ProductAddComponent implements OnInit {
+  productAddForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private toastrService: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.createProductAddForm();
+  }
+
+  createProductAddForm() {
+    this.productAddForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      unitPrice: ['', Validators.required],
+      unitsInStock: ['', Validators.required],
+      categoryId: ['', Validators.required],
+    });
+  }
+
+  add() {
+    if (this.productAddForm.valid) {
+      let productModel = Object.assign({}, this.productAddForm.value);
+      this.productService.add(productModel).subscribe(
+        (response) => {
+          this.toastrService.success(response.message, 'Success');
+        },
+        (errorResponse) => {
+          if (errorResponse.error.Errors.length > 0) {
+            for (let i = 0; i < errorResponse.error.Errors.length; i++) {
+              this.toastrService.error(
+                errorResponse.error.Errors[i].ErrorMessage,
+                'Error'
+              );
+            }
+          }
+        }
+      );
+    } else {
+      this.toastrService.error('Form is not valid');
+    }
+  }
+}
